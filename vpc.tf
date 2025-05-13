135314d66057030a00050d99a53030e9b49cb84e
Moulahusaingvt <156436466+Moulahusaingvt@users.noreply.github.com>
2025-05-13 12:08:21 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "hcl_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
+  vpc_id            = aws_vpc.hcl_vpc.id
   cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
-resource "aws_security_group" "lambda_sg" {
+resource "aws_security_group" "hcl_sg" {
   vpc_id = aws_vpc.my_vpc.id
   ingress {
     from_port   = 0
