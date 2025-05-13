2d9b8184d39ea2ba489b18d5563072b24fbf2369
Moulahusaingvt <156436466+Moulahusaingvt@users.noreply.github.com>
2025-05-13 11:24:11 +0530

    Create IAM.tf

---
+++ IAM.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "lambda-logs"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  roles      = [aws_iam_role.lambda_role.name]
+}
