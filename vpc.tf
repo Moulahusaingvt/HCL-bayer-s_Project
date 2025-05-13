05a6cb4d33b93bd877912facb546b5980be5a1c8
Moulahusaingvt <156436466+Moulahusaingvt@users.noreply.github.com>
2025-05-13 11:23:35 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
