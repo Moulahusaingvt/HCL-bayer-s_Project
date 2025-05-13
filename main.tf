1bdaac978058de8548ad5910bff26066db2d27e7
Moulahusaingvt <156436466+Moulahusaingvt@users.noreply.github.com>
2025-05-13 17:29:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,9 @@ provider "aws" {
 resource "aws_instance" "jenkinsmaster" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  count = 2
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "HCLInstance"
   }
 }
